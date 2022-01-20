882b23b29a8022f76427be2fe434576346d866d4
Kehinde Obe <48479284+kennyobey@users.noreply.github.com>
2022-01-20 10:07:19 -0800

    Alert Dialog

--- lib/Screens/Alet dialogue/alertdialogue.dart
+++ lib/Screens/Alet dialogue/alertdialogue.dart
@@ -1,4 +1,3 @@
-
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, deprecated_member_use, prefer_const_constructors_in_immutables
 
 import 'dart:ui';
@@ -9,8 +8,12 @@ import 'package:studo/Screens/Alet%20dialogue/constant.dart';
 class CustomDialogBox extends StatefulWidget {
   final String title, descriptions, text;
 
-
-   CustomDialogBox({Key? key, required this.title, required this.descriptions, required this.text}) : super(key: key);
+  CustomDialogBox(
+      {Key? key,
+      required this.title,
+      required this.descriptions,
+      required this.text})
+      : super(key: key);
 
   @override
   _CustomDialogBoxState createState() => _CustomDialogBoxState();
@@ -20,120 +23,197 @@ class _CustomDialogBoxState extends State<CustomDialogBox> {
   @override
   Widget build(BuildContext context) {
     return Dialog(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(Constants.padding),
-        ),
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-        child: contentBox(context),
-      
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(Constants.padding),
+      ),
+      elevation: 0,
+      backgroundColor: Colors.transparent,
+      child: contentBox(context),
     );
   }
-  contentBox(context){
-    return Stack(
-      children: <Widget>[
-        Container(
-          padding: EdgeInsets.only(left: Constants.padding,top: Constants.avatarRadius
-              + Constants.padding, right: Constants.padding,bottom: Constants.padding
-          ),
-          margin: EdgeInsets.only(top: Constants.avatarRadius),
-          decoration: BoxDecoration(
-            shape: BoxShape.rectangle,
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(Constants.padding),
-            boxShadow: [
-              BoxShadow(color: Colors.black,offset: Offset(0,5),
-              blurRadius: 5
-              ),
-            ]
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Row(
-              children: [
-              Container(  
-              margin: EdgeInsets.all(3),  
-              child: FlatButton(  
-                child: Text('Details', style: TextStyle(fontSize: 15.0),),  
-                color: Color.fromRGBO(25, 50, 80, 1),  
-                textColor: Colors.white,  
-                onPressed: () {},  
-              ),  
-            ), 
-            Container(  
-              
-              margin: EdgeInsets.all(3),  
-              child: FlatButton(  
-                child: Text('Tags', style: TextStyle(fontSize: 15.0),),  
-                color: Color.fromRGBO(25, 50, 80, 1),  
-                textColor: Colors.white,  
-                onPressed: () {},  
-              ),  
-            ),
-            Container(  
-              margin: EdgeInsets.all(3),  
-              child: FlatButton(  
-                child: Text('Deadline', style: TextStyle(fontSize: 15.0),),  
-                color: Color.fromRGBO(25, 50, 80, 1),  
-                textColor: Colors.white,  
-                onPressed: () {},  
-              ),  
-            ), 
 
-            ],),
-              TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Title',
-                    labelStyle: TextStyle(
-                        fontFamily: 'Montserrat',
-                        fontWeight: FontWeight.bold,
-                        color: Colors.grey,
-                        fontSize: 15),
-                    focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Color.fromRGBO(25, 50, 80, 1),)),
-                  ),
-                  onChanged: (text){
-                     // var name = text;
-                  },
+  contentBox(context) {
+    return MaterialApp(
+        home: DefaultTabController(
+            length: 3,
+            child: Scaffold(
+              appBar: AppBar(
+                bottom: TabBar(
+                  tabs: [
+                    Tab(icon: Icon(Icons.directions_car)),
+                    Tab(icon: Icon(Icons.directions_transit)),
+                    Tab(icon: Icon(Icons.directions_bike)),
+                  ],
                 ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Description',
-                    labelStyle: TextStyle(
-                        fontFamily: 'Montserrat',
-                        fontWeight: FontWeight.bold,
-                        color: Colors.grey,
-                        fontSize: 15),
-                    focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Color.fromRGBO(25, 50, 80, 1),)),
-                  ),
-                  onChanged: (text){
-                     // var name = text;
-                  },
+              ),
+              body: TabBarView(children: [
+                Container(
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: 16,
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Even Densed TextFiled',
+                            isDense: true, // Added this
+                            contentPadding: EdgeInsets.all(8), // Added this
+                          ),
+                        ),
+                      ]),
                 ),
-                Align(
-                alignment: Alignment.bottomLeft,
-                child: FlatButton(
-                    onPressed: (){
-                      Navigator.of(context).pop();
-                    },
-                    child: Text("Submit", style: TextStyle(fontSize: 18),)),
-              ),
-              
-              Align(
-                alignment: Alignment.bottomRight,
-                child: FlatButton(
-                    onPressed: (){
-                      Navigator.of(context).pop();
-                    },
-                    child: Text("Submit", style: TextStyle(fontSize: 18),)),
-              ),
-            ],
-          ),
-        ),
-       
-      ],
-    );
+                Container(
+                    padding: EdgeInsets.all(10),
+                    child: Column(children: [
+                      Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              height: 16,
+                            ),
+                            TextField(
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'Even Densed TextFiled',
+                                isDense: true, // Added this
+                                contentPadding: EdgeInsets.all(8), // Added this
+                              ),
+                            ),
+                          ])
+                    ])),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  height: 200,
+                  width: 200,
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 16,
+                      ),
+                      Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            TextField(
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'Even Densed TextFiled',
+                                isDense: true, // Added this
+                                contentPadding: EdgeInsets.all(8), // Added this
+                              ),
+                            ),
+                          ]),
+                    ],
+                  ),
+                ),
+              ]),
+            )));
+
+    //  Stack(
+    //   children: <Widget>[
+    //     Container(
+    //       padding: EdgeInsets.only(left: Constants.padding,top: Constants.avatarRadius
+    //           + Constants.padding, right: Constants.padding,bottom: Constants.padding
+    //       ),
+    //       margin: EdgeInsets.only(top: Constants.avatarRadius),
+    //       decoration: BoxDecoration(
+    //         shape: BoxShape.rectangle,
+    //         color: Colors.white,
+    //         borderRadius: BorderRadius.circular(Constants.padding),
+    //         boxShadow: [
+    //           BoxShadow(color: Colors.black,offset: Offset(0,5),
+    //           blurRadius: 5
+    //           ),
+    //         ]
+    //       ),
+    //       child: Column(
+    //         mainAxisSize: MainAxisSize.min,
+    //         children: <Widget>[
+    //           Row(
+    //           children: [
+    //           Container(
+    //           margin: EdgeInsets.all(3),
+    //           child: FlatButton(
+    //             child: Text('Details', style: TextStyle(fontSize: 15.0),),
+    //             color: Color.fromRGBO(25, 50, 80, 1),
+    //             textColor: Colors.white,
+    //             onPressed: () {},
+    //           ),
+    //         ),
+    //         Container(
+
+    //           margin: EdgeInsets.all(3),
+    //           child: FlatButton(
+    //             child: Text('Tags', style: TextStyle(fontSize: 15.0),),
+    //             color: Color.fromRGBO(25, 50, 80, 1),
+    //             textColor: Colors.white,
+    //             onPressed: () {},
+    //           ),
+    //         ),
+    //         Container(
+    //           margin: EdgeInsets.all(3),
+    //           child: FlatButton(
+    //             child: Text('Deadline', style: TextStyle(fontSize: 15.0),),
+    //             color: Color.fromRGBO(25, 50, 80, 1),
+    //             textColor: Colors.white,
+    //             onPressed: () {},
+    //           ),
+    //         ),
+
+    //         ],),
+    //           TextFormField(
+    //               decoration: InputDecoration(
+    //                 labelText: 'Title',
+    //                 labelStyle: TextStyle(
+    //                     fontFamily: 'Montserrat',
+    //                     fontWeight: FontWeight.bold,
+    //                     color: Colors.grey,
+    //                     fontSize: 15),
+    //                 focusedBorder: UnderlineInputBorder(
+    //                     borderSide: BorderSide(color: Color.fromRGBO(25, 50, 80, 1),)),
+    //               ),
+    //               onChanged: (text){
+    //                  // var name = text;
+    //               },
+    //             ),
+    //             TextFormField(
+    //               decoration: InputDecoration(
+    //                 labelText: 'Description',
+    //                 labelStyle: TextStyle(
+    //                     fontFamily: 'Montserrat',
+    //                     fontWeight: FontWeight.bold,
+    //                     color: Colors.grey,
+    //                     fontSize: 15),
+    //                 focusedBorder: UnderlineInputBorder(
+    //                     borderSide: BorderSide(color: Color.fromRGBO(25, 50, 80, 1),)),
+    //               ),
+    //               onChanged: (text){
+    //                  // var name = text;
+    //               },
+    //             ),
+    //             Align(
+    //             alignment: Alignment.bottomLeft,
+    //             child: FlatButton(
+    //                 onPressed: (){
+    //                   Navigator.of(context).pop();
+    //                 },
+    //                 child: Text("Submit", style: TextStyle(fontSize: 18),)),
+    //           ),
+
+    //           Align(
+    //             alignment: Alignment.bottomRight,
+    //             child: FlatButton(
+    //                 onPressed: (){
+    //                   Navigator.of(context).pop();
+    //                 },
+    //                 child: Text("Submit", style: TextStyle(fontSize: 18),)),
+    //           ),
+    //         ],
+    //       ),
+    //     ),
+
+    //   ],
+    // );
   }
 }
