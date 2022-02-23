4262b28434735e9e60874d6deb8ba1eddc6593da
michtimiael <michtimiael@gmail.com>
2022-02-23 15:03:37 +0100

    setting up auth

--- lib/services/repository/auth_repository.dart
+++ lib/services/repository/auth_repository.dart
@@ -1,3 +1,18 @@
 import 'package:get/get.dart';
 
-class AuthenticationRepository extends GetxController {}
+enum Status {
+  Loading,
+  Authenticated,
+  Unauthenticated,
+  Error,
+  Empty,
+}
+
+class AuthenticationRepository extends GetxController {
+  final _errorMessage = "".obs;
+  final token = "".obs;
+  final _status = Status.Empty.obs;
+
+  String get errorMessage => _errorMessage.value;
+  Status get status => _status.value;
+}
